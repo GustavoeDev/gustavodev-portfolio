ac5eaad75e5aa05fb09353a6277fa45a5e76389d
Gustavo Emanuel <gustavoe.dev@gmail.com>
2025-01-11 14:24:14 -0300

    feat: add AnimationHover component and link hover effect styles

---
+++ src/components/shared/animation-hover.tsx
@@ -0,0 +1,49 @@
+import { useState } from "react";
+
+interface AnimationHoverProps {
+  as?: React.ElementType;
+  factor?: number;
+  href?: string;
+  children: React.ReactNode;
+  target?: string;
+  className?: string;
+}
+
+export default function AnimationHover({
+  as: Component = "a",
+  factor = 0.3,
+  children,
+  href,
+  target,
+  className,
+  ...props
+}: AnimationHoverProps) {
+  const [offset, setOffset] = useState({ x: 0, y: 0 });
+
+  const handleMouseMove = (e: React.MouseEvent<HTMLElement>) => {
+    const { clientX, clientY, currentTarget } = e;
+    const { left, top, width, height } = currentTarget.getBoundingClientRect();
+
+    const x = (clientX - left - width / 2) * factor;
+    const y = (clientY - top - height / 2) * factor;
+
+    setOffset({ x, y });
+  };
+
+  const resetOffset = () => setOffset({ x: 0, y: 0 });
+
+  return (
+    <Component
+      onMouseMove={handleMouseMove}
+      onMouseLeave={resetOffset}
+      className="inline-block"
+      style={{ transform: `translate(${offset.x}px, ${offset.y}px)` }}
+      {...(href && { href })}
+      {...(target && { target })}
+      {...(className && { className })}
+      {...props}
+    >
+      {children}
+    </Component>
+  );
+}
