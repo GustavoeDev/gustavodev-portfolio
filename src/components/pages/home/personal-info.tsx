4007d854c1a42c8118c3661011da9792ee7a866f
Gustavo Emanuel <gustavoe.dev@gmail.com>
2025-01-11 23:14:58 -0300

    feat: add PersonalInfo component to display user location and role

---
+++ src/components/pages/home/personal-info.tsx
@@ -0,0 +1,25 @@
+import { HiGlobeAlt } from "react-icons/hi";
+import { HiArrowDownRight } from "react-icons/hi2";
+
+export default function PersonalInfo() {
+  return (
+    <div className="absolute top-[25%] left-0 w-full flex items-end justify-between text-textWhite font-medium">
+      <div className="flex items-center justify-between gap-8 pl-8 pr-4 py-4 rounded-tr-[999px] rounded-br-[999px] bg-grayDark">
+        <div>
+          <p>Pau dos Ferros</p>
+          <p>Rio Grande do Norte</p>
+        </div>
+        <span className="p-4 rounded-[999px] inline-flex items-center justify-center dark:bg-grayBackground-dark bg-grayBackground-light">
+          <HiGlobeAlt size={32} />
+        </span>
+      </div>
+      <div className="mr-8 flex flex-col items-start gap-16">
+        <HiArrowDownRight size={28} />
+        <div className="flex flex-col gap-1 text-3xl">
+          <p>Desenvolvedor</p>
+          <p>FrontEnd</p>
+        </div>
+      </div>
+    </div>
+  );
+}
