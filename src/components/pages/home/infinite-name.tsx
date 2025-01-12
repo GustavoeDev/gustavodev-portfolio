709d70326e9918344b14d498306fe2a7daa23f08
Gustavo Emanuel <gustavoe.dev@gmail.com>
2025-01-11 23:13:43 -0300

    fix: update text color in InfiniteName component for better visibility

--- src/components/pages/home/infinite-name.tsx
+++ src/components/pages/home/infinite-name.tsx
@@ -1,7 +1,7 @@
 export default function InfiniteName() {
   return (
     <div className="overflow-hidden whitespace-nowrap">
-      <div className="inline-block text-[max(9em,15vw)] text-foreground animate-slide">
+      <div className="inline-block text-[max(9em,15vw)] text-textWhite animate-slide">
         <span className="mr-12">Gustavo Emanuel -</span>
         <span className="mr-12">Gustavo Emanuel -</span>
       </div>
