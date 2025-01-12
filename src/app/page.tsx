876b1b7fc03b8e63b07b1eddceea34f462b23704
Gustavo Emanuel <gustavoe.dev@gmail.com>
2025-01-11 23:15:02 -0300

    feat: integrate PersonalInfo and InfiniteName components into Home page layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,12 @@
+import InfiniteName from "@/components/pages/home/infinite-name";
+import PersonalInfo from "@/components/pages/home/personal-info";
+
 export default function Home() {
   return (
     <main>
-      <div className="h-screen bg-card bg-myPhotoImg bg-contain bg-no-repeat bg-center">
-        oi
+      <div className="h-screen dark:bg-grayBackground-dark bg-grayBackground-light bg-myPhotoImg bg-contain bg-no-repeat bg-center flex justify-end items-end ">
+        <PersonalInfo />
+        <InfiniteName />
       </div>
     </main>
   );
