f707ed0e286d413d87d9ff06a6b6c027041a3208
Gustavo Emanuel <gustavoe.dev@gmail.com>
2025-01-16 13:24:20 -0300

    feat: integrate Intro component into Home page layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import InfiniteName from "@/components/pages/home/infinite-name";
+import Intro from "@/components/pages/home/intro";
 import PersonalInfo from "@/components/pages/home/personal-info";
 
 export default function Home() {
@@ -7,6 +8,9 @@ export default function Home() {
       <div className="h-screen dark:bg-grayBackground-dark bg-grayBackground-light bg-myPhotoImg bg-contain bg-no-repeat bg-center flex flex-col justify-end ">
         <PersonalInfo />
         <InfiniteName />
+      </div>
+      <div className="h-screen flex items-center justify-center gap-12 w-full mx-auto bg-[url('/bg-portfolio.png')] bg-cover bg-center">
+        <Intro />
       </div>
     </main>
   );
