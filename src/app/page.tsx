9bebf85430e316adea65119ce94404a3b1266e79
Gustavo Emanuel <gustavoe.dev@gmail.com>
2025-01-15 21:00:13 -0300

    feat: update layout structure in Home component for improved responsiveness

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import PersonalInfo from "@/components/pages/home/personal-info";
 export default function Home() {
   return (
     <main>
-      <div className="h-screen dark:bg-grayBackground-dark bg-grayBackground-light bg-myPhotoImg bg-contain bg-no-repeat bg-center flex justify-end items-end ">
+      <div className="h-screen dark:bg-grayBackground-dark bg-grayBackground-light bg-myPhotoImg bg-contain bg-no-repeat bg-center flex flex-col justify-end ">
         <PersonalInfo />
         <InfiniteName />
       </div>
