6c25fb297dee26c5c27c19efa74aba390741808b
Gustavo Emanuel <gustavoe.dev@gmail.com>
2025-01-17 01:03:28 -0300

    feat: integrate RecentProjects component into Home page layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,9 @@
 import InfiniteName from "@/components/pages/home/infinite-name";
 import Intro from "@/components/pages/home/intro";
 import PersonalInfo from "@/components/pages/home/personal-info";
+import Project from "@/components/shared/project";
+import AnimationHover from "@/components/shared/animation-hover";
+import RecentProjects from "@/components/pages/home/recent-projects";
 
 export default function Home() {
   return (
@@ -11,6 +14,13 @@ export default function Home() {
       </div>
       <div className="h-screen flex items-center justify-center gap-12 w-full mx-auto bg-[url('/bg-portfolio.png')] bg-cover bg-center">
         <Intro />
+      </div>
+      <div className="min-h[100vh] max-w-[70rem] mx-auto bg-[url('/bg-portfolio.png')] bg-cover bg-center">
+        <h2 className="text-4xl max-w-[750px] font-title font-medium">
+          Na intersecção entre criatividade e tecnologia, desenvolvo soluções
+          web que transcendem o comum
+        </h2>
+        <RecentProjects />
       </div>
     </main>
   );
