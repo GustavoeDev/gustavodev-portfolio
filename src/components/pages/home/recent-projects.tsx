55a7cc94e5e1cf97002f1890ed722ac2a3fc5485
Gustavo Emanuel <gustavoe.dev@gmail.com>
2025-01-17 01:03:25 -0300

    feat: add RecentProjects component to display recent projects with hover effect

---
+++ src/components/pages/home/recent-projects.tsx
@@ -0,0 +1,28 @@
+import AnimationHover from "@/components/shared/animation-hover";
+import Project from "@/components/shared/project";
+
+export default function RecentProjects() {
+  return (
+    <div className="mt-28">
+      <p className="mb-8 text-gray-400 dark:text-gray-500 text-xs font-medium">
+        PROJETOS RECENTES
+      </p>
+      <hr className="border-t border-gray-200 dark:border-gray-500 pointer-events-none" />
+      <div className="">
+        <Project />
+        <Project />
+        <Project />
+      </div>
+      <div className="flex justify-center items-center my-16">
+        <AnimationHover
+          as="button"
+          className="p-5 z-[50px] border border-gray-200 dark:border-gray-500 overflow-hidden dark:text-gray-300 dark:hover:text-textWhite hover:text-textWhite transition-colors duration-300 rounded-full cursor-pointer before:content-[''] before:absolute before:inset-0 before:bg-blue-500 before:transition-[clip-path] before:duration-500 before:ease-in-out before:[clip-path:ellipse(20%_0%_at_50%_0%)] hover:before:[clip-path:ellipse(150%_150%_at_50%_0%)]"
+        >
+          <p className="relative z-[999px] flex gap-1">
+            Mais projetos <span className="text-[10px]">18</span>
+          </p>
+        </AnimationHover>
+      </div>
+    </div>
+  );
+}
