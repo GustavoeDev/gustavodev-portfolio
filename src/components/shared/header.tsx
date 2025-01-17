72f137c1c0e7d58855cb113aef353cdec0463757
Gustavo Emanuel <gustavoe.dev@gmail.com>
2025-01-17 15:55:19 -0300

    feat: integrate NavigationContext in Header component for mobile navigation toggle

--- src/components/shared/header.tsx
+++ src/components/shared/header.tsx
@@ -3,24 +3,49 @@
 import Link from "next/link";
 import AnimationHover from "./animation-hover";
 import { ModeToggle } from "./theme-toggle";
+import { NavigationContext } from "@/contexts/openNavigationContext";
+import { useContext } from "react";
 
 export default function Header() {
+  const { handleOpenNavigation } = useContext(NavigationContext);
+
   return (
     <header className="w-full p-8 flex items-center justify-between text-base absolute text-textWhite">
       <AnimationHover as={Link} href="/">
         © Feito por Gustavo
       </AnimationHover>
       <nav className="flex items-center gap-8">
-        <AnimationHover as={Link} href="/projects" className="link-with-hover">
+        <AnimationHover
+          as={Link}
+          href="/projects"
+          className="link-with-hover max-sm:hidden"
+        >
           Projetos
         </AnimationHover>
-        <AnimationHover as={Link} href="/about" className="link-with-hover">
+        <AnimationHover
+          as={Link}
+          href="/about"
+          className="link-with-hover max-sm:hidden"
+        >
           Sobre mim
         </AnimationHover>
-        <AnimationHover as={Link} href="/contact" className="link-with-hover">
+        <AnimationHover
+          as={Link}
+          href="/contact"
+          className="link-with-hover max-sm:hidden"
+        >
           Contato
         </AnimationHover>
-        <ModeToggle />
+        <AnimationHover
+          as="button"
+          className="link-with-hover hidden max-sm:block"
+          onClick={() => handleOpenNavigation(true)}
+        >
+          Menu
+        </AnimationHover>
+        <div className="max-sm:hidden">
+          <ModeToggle />
+        </div>
       </nav>
     </header>
   );
