dd2ff8c47383dfaf14ca5417148bed62328fdd0e
Gustavo Emanuel <gustavoe.dev@gmail.com>
2025-01-17 00:58:33 -0300

    feat: refactor AnimationHover component to use forwardRef for improved flexibility

--- src/components/shared/animation-hover.tsx
+++ src/components/shared/animation-hover.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState } from "react";
+import { useState, forwardRef } from "react";
 
 interface AnimationHoverProps {
   as?: React.ElementType;
@@ -13,45 +13,54 @@ interface AnimationHoverProps {
   rel?: string;
 }
 
-export default function AnimationHover({
-  as: Component = "a",
-  factor = 0.2,
-  children,
-  href,
-  target,
-  className,
-  onClick,
-  rel,
-  ...props
-}: AnimationHoverProps) {
-  const [offset, setOffset] = useState({ x: 0, y: 0 });
+const AnimationHover = forwardRef<HTMLElement, AnimationHoverProps>(
+  (
+    {
+      as: Component = "a",
+      factor = 0.2,
+      children,
+      href,
+      target,
+      className,
+      onClick,
+      rel,
+      ...props
+    }: AnimationHoverProps,
+    ref
+  ) => {
+    const [offset, setOffset] = useState({ x: 0, y: 0 });
 
-  const handleMouseMove = (e: React.MouseEvent<HTMLElement>) => {
-    const { clientX, clientY, currentTarget } = e;
-    const { left, top, width, height } = currentTarget.getBoundingClientRect();
+    const handleMouseMove = (e: React.MouseEvent<HTMLElement>) => {
+      const { clientX, clientY, currentTarget } = e;
+      const { left, top, width, height } =
+        currentTarget.getBoundingClientRect();
 
-    const x = (clientX - left - width / 2) * factor;
-    const y = (clientY - top - height / 2) * factor;
+      const x = (clientX - left - width / 2) * factor;
+      const y = (clientY - top - height / 2) * factor;
 
-    setOffset({ x, y });
-  };
+      setOffset({ x, y });
+    };
 
-  const resetOffset = () => setOffset({ x: 0, y: 0 });
+    const resetOffset = () => setOffset({ x: 0, y: 0 });
 
-  return (
-    <Component
-      onMouseMove={handleMouseMove}
-      onMouseLeave={resetOffset}
-      className="inline-block"
-      style={{ transform: `translate(${offset.x}px, ${offset.y}px)` }}
-      {...(href && { href })}
-      {...(target && { target })}
-      {...(className && { className })}
-      {...(onClick && { onClick })}
-      {...(rel && { rel })}
-      {...props}
-    >
-      {children}
-    </Component>
-  );
-}
+    return (
+      <Component
+        onMouseMove={handleMouseMove}
+        onMouseLeave={resetOffset}
+        className="inline-block"
+        style={{ transform: `translate(${offset.x}px, ${offset.y}px)` }}
+        ref={ref}
+        {...(href && { href })}
+        {...(target && { target })}
+        {...(className && { className })}
+        {...(onClick && { onClick })}
+        {...(rel && { rel })}
+        {...props}
+      >
+        {children}
+      </Component>
+    );
+  }
+);
+
+export default AnimationHover;
