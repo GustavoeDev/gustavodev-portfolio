bc1f3aceeab6481a52f983a5990a647ff8a65034
Gustavo Emanuel <gustavoe.dev@gmail.com>
2025-01-18 18:08:13 -0300

    feat: add Skills component to showcase programming languages and frameworks

---
+++ src/components/pages/about/skills.tsx
@@ -0,0 +1,60 @@
+export default function Skills() {
+  return (
+    <div className="grid grid-cols-3 gap-16">
+      <div className="flex flex-col gap-6">
+        <div>
+          <p className="mb-6 text-zinc-400 dark:text-zinc-500 text-sm font-medium">
+            01
+          </p>
+          <hr className="border-t border-zinc-200 dark:border-zinc-500 pointer-events-none" />
+        </div>
+        <span className="text-2xl">Next.js</span>
+      </div>
+      <div className="flex flex-col gap-6">
+        <div>
+          <p className="mb-6 text-zinc-400 dark:text-zinc-500 text-sm font-medium">
+            02
+          </p>
+          <hr className="border-t border-zinc-200 dark:border-zinc-500 pointer-events-none" />
+        </div>
+        <span className="text-2xl">React.js</span>
+      </div>
+      <div className="flex flex-col gap-6">
+        <div>
+          <p className="mb-6 text-zinc-400 dark:text-zinc-500 text-sm font-medium">
+            03
+          </p>
+          <hr className="border-t border-zinc-200 dark:border-zinc-500 pointer-events-none" />
+        </div>
+        <span className="text-2xl">Typescript</span>
+      </div>
+      <div className="flex flex-col gap-6">
+        <div>
+          <p className="mb-6 text-zinc-400 dark:text-zinc-500 text-sm font-medium">
+            04
+          </p>
+          <hr className="border-t border-zinc-200 dark:border-zinc-500 pointer-events-none" />
+        </div>
+        <span className="text-2xl">Javascript</span>
+      </div>
+      <div className="flex flex-col gap-6">
+        <div>
+          <p className="mb-6 text-zinc-400 dark:text-zinc-500 text-sm font-medium">
+            05
+          </p>
+          <hr className="border-t border-zinc-200 dark:border-zinc-500 pointer-events-none" />
+        </div>
+        <span className="text-2xl">HTML + CSS</span>
+      </div>
+      <div className="flex flex-col gap-6">
+        <div>
+          <p className="mb-6 text-zinc-400 dark:text-zinc-500 text-sm font-medium">
+            06
+          </p>
+          <hr className="border-t border-zinc-200 dark:border-zinc-500 pointer-events-none" />
+        </div>
+        <span className="text-2xl">Python + Django</span>
+      </div>
+    </div>
+  );
+}
