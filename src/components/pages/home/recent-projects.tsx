e2a48586746480a7b061c1f8228668df29dfbc8a
Gustavo Emanuel <gustavoe.dev@gmail.com>
2025-01-18 18:07:26 -0300

    feat: update RecentProjects component to use Link for navigation to projects page

--- src/components/pages/home/recent-projects.tsx
+++ src/components/pages/home/recent-projects.tsx
@@ -1,5 +1,6 @@
 import AnimationHover from "@/components/shared/animation-hover";
 import Project from "@/components/shared/project";
+import Link from "next/link";
 
 export default function RecentProjects() {
   return (
@@ -15,7 +16,8 @@ export default function RecentProjects() {
       </div>
       <div className="flex justify-center items-center my-16">
         <AnimationHover
-          as="button"
+          as={Link}
+          href="/projects"
           className="p-5 z-[50px] border border-zinc-200 dark:border-zinc-500 overflow-hidden dark:text-zinc-300 dark:hover:text-textWhite hover:text-textWhite transition-colors duration-300 rounded-full cursor-pointer before:content-[''] before:absolute before:inset-0 before:bg-blue-500 before:transition-[clip-path] before:duration-500 before:ease-in-out before:[clip-path:ellipse(20%_0%_at_50%_0%)] hover:before:[clip-path:ellipse(150%_150%_at_50%_0%)]"
         >
           <p className="relative z-[999px] flex gap-1">
